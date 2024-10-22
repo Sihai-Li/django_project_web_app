352afda807875e7519fe970c4cdeee331b461004
Sihai Li <shli2020@outlook.com>
2024-10-22 04:22:47 -0500

    update

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers  import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
 
 
 # Create your views here.
